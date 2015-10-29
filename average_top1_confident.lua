7781d2dfa06d8c51a9c5ae0f0632596122436509
meteora9479 <meteora9479@gmail.com>
2015-10-29 20:10:45 +0800

    first commit

---
+++ average_top1_confident.lua
@@ -0,0 +1,63 @@
+require 'cudnn'
+require 'inn'
+require 'image'
+require 'dcnn'
+
+-- Loads the mapping from net outputs to human readable labels
+function load_synset()
+  local file = io.open 'synset_words.txt'
+  local list = {}
+  while true do
+    local line = file:read()
+    if not line then break end
+    table.insert(list, string.sub(line,11))
+  end
+  return list
+end
+
+-- Converts an image from RGB to BGR format and subtracts mean
+function preprocess(im, img_mean)
+  -- rescale the image
+  local im3 = image.scale(im,224,224,'bilinear')*255
+  -- RGB2BGR
+  local im4 = im3:clone()
+  im4[{1,{},{}}] = im3[{3,{},{}}]
+  im4[{3,{},{}}] = im3[{1,{},{}}]
+
+  -- subtract imagenet mean
+  return im4 - image.scale(img_mean, 224, 224, 'bilinear')
+end
+
+-- Setting up networks 
+print '==> Loading network'
+
+--net = torch.load('/home/yusheng/Workspace/DeepLearning/models/zeilerNet/zeilerNet.net')
+net = torch.load('/usr/local/data/zeilerNet/zeilerNet.net')
+net:cuda()
+-- as we want to classify, let's disable dropouts by enabling evaluation mode
+net:evaluate()
+
+print '==> Loading synsets'
+synset_words = load_synset()
+
+print '==> Loading image and imagenet mean'
+--image_name = 'Goldfish3.jpg'
+--image_name = 'lena.jpg'
+image_name='people2.jpg'
+img_mean_name = 'ilsvrc_2012_mean.t7'
+
+im = image.load(image_name)
+img_mean = torch.load(img_mean_name).img_mean:transpose(3,1)
+
+-- Have to resize and convert from RGB to BGR and subtract mean
+print '==> Preprocessing'
+I = preprocess(im, img_mean)
+
+-- Replace pooling by dual pooling
+unpooling_layers = dcnn:ReplaceDualPoolingModule(net:get(1))
+
+_,classes = net:forward(I:cuda()):view(-1):float():sort(true)
+
+for i=1,5 do
+  print('predicted class '..tostring(i)..': ', synset_words[classes[i] ])
+end
